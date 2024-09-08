c5e856580cd5ad8f314b26b0f35b06e6a99efce4
yeahross <yeahross@protonmail.com>
2024-09-08 20:36:02 +0100

    Add buildMidiRecordFile

--- mio-midi/src/midi.js
+++ mio-midi/src/midi.js
@@ -67,7 +67,7 @@ const instrumentLengths = [
 	16, 48, 16, 4,
 	32, 32, 32, 3,
 	// Group 3: SF: Future and Mystery
-	64, 32, 32, 3,
+	64, 64, 32, 3,
 	32, 32, 16, 4,
 	// Group 4: Creatures/Animals
 	16, 16, 16, 16,
@@ -76,8 +76,8 @@ const instrumentLengths = [
 	32, 24, 24, 32,
 	24, 32, 32, 48,
 	// Group 6: 8-bit
-	8, 9, 64, 16,
-	8, 5, 4, 8
+	32, 32, 64, 32,
+	32, 32, 32, 32
 ];
 
 const basePitches = [
@@ -100,6 +100,11 @@ const basePitches = [
 	3, 3, 3, 3,
 	3, 3, 3, 3,
 ];
+
+const drumCodes = [
+	0, 25, 1, 56,
+	48, 20, 24, 16
+]
 
 const buildMidiFile = (mioData, loopTimes = 0) => {
 	let tracks = [];
@@ -185,16 +190,16 @@ const buildMidiFile = (mioData, loopTimes = 0) => {
 		let drumSet = mioData[0xBA6F] & 0x7;
 
 		let drumConversion;
-		if (drumSet === 0) drumConversion = [35, 38, 42, 46, 49, 45, 50, 47, 31, 39, 82, 25, 80, 81];
+		if (drumSet === 0) drumConversion = [35, 38, 42, 46, 49, 45, 50, 47, 37, 39, 82, 29, 80, 81];
 		if (drumSet === 1) drumConversion = [35, 38, 44, 46, 49, 40, 41, 42, 39, 37, 50, 51, 81, 80];
-		if (drumSet === 2) drumConversion = [66, 65, 82, 56, 57, 61, 60, 62, 75, 58, 79, 78, 81, 72];
+		if (drumSet === 2) drumConversion = [66, 65, 82, 56, 57, 61, 60, 62, 75, 58, 79, 78, 83, 72];
 		if (drumSet === 3) drumConversion = [36, 37, 38, 50, 40, 60, 61, 62, 44, 39, 46, 47, 49, 51];
 		if (drumSet === 4) drumConversion = [36, 37, 82, 81, 38, 40, 42, 47, 39, 34, 41, 43, 44, 80];
 		if (drumSet === 5) drumConversion = [36, 38, 80, 81, 39, 35, 40, 41, 42, 58, 44, 45, 49, 43];
 		if (drumSet === 6) drumConversion = [35, 38, 42, 46, 49, 40, 44, 39, 37, 51, 52, 53, 54, 55];
 		if (drumSet === 7) drumConversion = [35, 38, 42, 46, 49, 41, 45, 50, 36, 39, 43, 34, 47, 48];
 
-		track.addEvent(new MidiWriter.ProgramChangeEvent({ channel: 10, instrument: drumSet }));
+		track.addEvent(new MidiWriter.ProgramChangeEvent({ channel: 10, instrument: drumCodes[drumSet] }));
 		track.addEvent(new MidiWriter.ControllerChangeEvent({ channel: 10, controllerNumber: 10, controllerValue: pan }));
 
 		for (let loopIter = 0; loopIter <= loopTimes; loopIter++) {
@@ -238,20 +243,22 @@ window.buildMidiRecordFile = (mioData) => {
 	const BASE_DRUM_OFFSET = 0x187;
 	const BASE_DRUMSET_OFFSET = 0x211;
 	const TEMPO_OFFSET = 0x101;
+	const SWING_OFFSET = 0x100;
+	const swingJump = 11;
 
 	let tracks = [];
 
 	let trackLength = 32;
 
 	let tempo = mioData[TEMPO_OFFSET] * 10 + 60;
-	console.log(tempo);
+	let isSwing = mioData[SWING_OFFSET] != 0;
+	console.log("Tempo:", tempo, "Swing:", isSwing);
 
 	for (let trackIndex = 0; trackIndex < TRACK_COUNT; trackIndex++) {
+		let track = new MidiWriter.Track();
+		track.setTempo(tempo);
+		let lastTick = 0;
 		for (let segmentIndex = 0; segmentIndex < mioData[END_INDEX]; segmentIndex++) {
-
-			let track = new MidiWriter.Track();
-
-			track.setTempo(tempo);
 
 			let songOffset = BASE_SONG_OFFSET + SEGMENT_OFFSET(segmentIndex) + trackIndex * trackLength;
 
@@ -260,7 +267,6 @@ window.buildMidiRecordFile = (mioData) => {
 			let pan = Math.min(127, mioData[BASE_PAN_OFFSET + SEGMENT_OFFSET(segmentIndex) + trackIndex] * PAN_MULTIPLIER);
 
 			let notesUsed = []
-			let wasNoteFound = false;
 
 			let base = basePitches[instrumentUsed];
 
@@ -276,17 +282,13 @@ window.buildMidiRecordFile = (mioData) => {
 
 			let channel = trackIndex + 1;
 
-			track.addEvent(new MidiWriter.ProgramChangeEvent({ channel, instrument: parseInt(instrumentCodes[instrumentUsed]) }));
+			track.addEvent(new MidiWriter.ProgramChangeEvent({ channel, instrument: parseInt(instrumentCodes[instrumentUsed])}));
 			track.addEvent(new MidiWriter.ControllerChangeEvent({ channel, controllerNumber: 10, controllerValue: pan }));
 			for (let i = 0; i < trackLength; i++) {
 				let note = mioData[songOffset + i];
 				if (note !== 255) {
 					wasNoteFound = true;
 					let noteLength = (instrumentLengths[instrumentUsed] * 8);
-					// Shortens high bingbings
-					if (instrumentUsed == 44 && note >= 19) {
-						noteLength = 4 * 8;
-					}
 					let si = segmentIndex;
 					for (var p = i + 1; ; p++) {
 						if (p % trackLength == 0) {
@@ -305,64 +307,75 @@ window.buildMidiRecordFile = (mioData) => {
 							break;
 						}
 					}
+					let swingOffset = isSwing && i % 2 == 1 ? swingJump : 0;
 					if (p != 0) {
 						let peek = p * 32;
+						let startPos = i * 32 + swingOffset;
+						if (noteLength > peek - startPos) {
+							noteLength = peek - startPos;
+						}
 						noteLength = Math.min(noteLength, peek - i * 32);
 					}
 					let startTick = 32 * i + segmentIndex * 1024;
+					if (isSwing && i % 2 == 1) {
+						startTick += swingOffset;
+					}
+					// console.debug(i, startTick, lastTick, startTick - lastTick)
 					let duration = 'T' + noteLength;
 					track.addEvent(new MidiWriter.NoteEvent({
 						pitch: notesUsed[note],
 						duration,
-						startTick,
+						wait: 'T' + (startTick - lastTick),
 						channel,
 						velocity: volume
 					}));
+
+					lastTick = startTick;
+					let lastLength = noteLength;
+					lastTick += lastLength;
 				}
 			}
 
-			if (wasNoteFound) {
-				tracks.push(track);
-			}
 		}
+		
+		tracks.push(track);
 	}
 
+	let track = new MidiWriter.Track();
+
+	track.setTempo(tempo);
 
 	for (let segmentIndex = 0; segmentIndex < mioData[END_INDEX]; segmentIndex++) {
-
-		let track = new MidiWriter.Track();
-
-		track.setTempo(tempo);
-
 		let volume = mioData[BASE_VOLUME_OFFSET + SEGMENT_OFFSET(segmentIndex) + 4] * VOLUME_MULTIPLIER;
 		let pan = Math.min(127, mioData[BASE_PAN_OFFSET + SEGMENT_OFFSET(segmentIndex) + 4] * PAN_MULTIPLIER);
 
 		let drumSet = mioData[BASE_DRUMSET_OFFSET + SEGMENT_OFFSET(segmentIndex) + 4] & 0x7;
 
 		let drumConversion;
-		if (drumSet === 0) drumConversion = [35, 38, 42, 46, 49, 45, 50, 47, 31, 39, 82, 25, 80, 81];
+		if (drumSet === 0) drumConversion = [35, 38, 42, 46, 49, 45, 50, 47, 37, 39, 82, 29, 80, 81];
 		if (drumSet === 1) drumConversion = [35, 38, 44, 46, 49, 40, 41, 42, 39, 37, 50, 51, 81, 80];
-		if (drumSet === 2) drumConversion = [66, 65, 82, 56, 57, 61, 60, 62, 75, 58, 79, 78, 81, 72];
+		if (drumSet === 2) drumConversion = [66, 65, 82, 56, 57, 61, 60, 62, 75, 58, 79, 78, 83, 72];
 		if (drumSet === 3) drumConversion = [36, 37, 38, 50, 40, 60, 61, 62, 44, 39, 46, 47, 49, 51];
 		if (drumSet === 4) drumConversion = [36, 37, 82, 81, 38, 40, 42, 47, 39, 34, 41, 43, 44, 80];
 		if (drumSet === 5) drumConversion = [36, 38, 80, 81, 39, 35, 40, 41, 42, 58, 44, 45, 49, 43];
 		if (drumSet === 6) drumConversion = [35, 38, 42, 46, 49, 40, 44, 39, 37, 51, 52, 53, 54, 55];
 		if (drumSet === 7) drumConversion = [35, 38, 42, 46, 49, 41, 45, 50, 36, 39, 43, 34, 47, 48];
 
-		track.addEvent(new MidiWriter.ProgramChangeEvent({ channel: 10, instrument: drumSet }));
+		track.addEvent(new MidiWriter.ProgramChangeEvent({ channel: 10, instrument: drumCodes[drumSet] }));
 		track.addEvent(new MidiWriter.ControllerChangeEvent({ channel: 10, controllerNumber: 10, controllerValue: pan }));
 
 		for (let i = 0; i < trackLength; i++) {
 			for (let drumIndex = 0; drumIndex < SIMULTANEOUS_DRUMS; drumIndex++) {
 				let drumUsed = mioData[BASE_DRUM_OFFSET + SEGMENT_OFFSET(segmentIndex) + i + drumIndex * 32];
 				if (drumUsed !== 255) {
-					let duration = 'T128';
+					let duration = 'T32';
+					let swingOffset = isSwing && i % 2 == 1 ? swingJump : 0;
 					track.addEvent([
 						new MidiWriter.NoteEvent({
 							pitch: drumConversion[drumUsed],
 							duration,
 							channel: 10,
-							startTick: 32 * i + segmentIndex * 1024,
+							startTick: 32 * i + segmentIndex * 1024 + swingOffset,
 							velocity: volume
 						}),
 					]);
@@ -370,12 +383,28 @@ window.buildMidiRecordFile = (mioData) => {
 			}
 		}
 
-		tracks.push(track);
+		let interim_write = new MidiWriter.Writer([track]);
+		interim_write.buildFile();
+		track = interim_write.tracks[0];
 	}
+
+	let lastOne = 0;
+	for (let z = 0; z < track.events.length; z++) {
+
+		if (Number.isNaN(track.events[z].delta)) {
+			track.events[z].delta = track.events[z].tick - lastOne;
+		}
+		if (track.events[z].tick) {
+			lastOne = track.events[z].tick;
+		}
+
+	}
+
+	tracks.push(track);
 
 	let write = new MidiWriter.Writer(tracks);
 
 	return write.buildFile()
 }
 
-module.exports = { buildMidiFile, buildRecordMidiFile };
+module.exports = { buildMidiFile, buildMidiRecordFile };
